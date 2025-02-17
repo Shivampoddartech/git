d651487a6c80146b96bfc65bd268e2a74f9746bf
shivam <shivampoddartech@gmail.com>
2025-02-17 15:42:19 +0530

    Modifying the user class

--- src/main/java/com/example/git_demo/model/User.java
+++ src/main/java/com/example/git_demo/model/User.java
@@ -3,6 +3,15 @@ package com.example.git_demo.model;
 public class User {
     private   int userId;
     private String userName;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public int getUserId() {
         return userId;
